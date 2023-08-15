d0ed26fe394198862322058593bcb52248ffc1f6
gsalmao <gustmsalmeron@gmail.com>
2023-08-15 10:55:14 -0300

    Update rifle aim positions

--- quantum_unity/Assets/Scripts/Debugging/PlayerAnimatorDebugger.cs
+++ quantum_unity/Assets/Scripts/Debugging/PlayerAnimatorDebugger.cs
@@ -14,6 +14,7 @@ namespace ZAMB.Debugging
         private const string moveValue = "MoveValue";
         private const string toStandard = "ToStandard";
         private const string toRifle = "ToRifle";
+        private const string rifle_Aim = "Rifle_Aim";
 
         protected override void Awake()
         {
@@ -29,6 +30,9 @@ namespace ZAMB.Debugging
 
         [Button("Lose guns")]
         private void LoseGuns() => _animator.SetTrigger(toStandard);
+
+        [Button("Aim")]
+        private void Aim() => _animator.Play(rifle_Aim);
 
 
     }
